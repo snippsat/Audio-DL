296e86d73a3bc3310410acb31a35b79460486ac5
Tom Egil Larsen <teglarse@online.no>
2024-04-09 12:51:27 +0200

    Update dt.py

--- dt.py
+++ dt.py
@@ -1,13 +1,15 @@
 import typer
 from yt_dlp import YoutubeDL
+from yt_dlp.utils import DownloadError
 
 app = typer.Typer()
 
 @app.command()
 def download_video(
     url: str,
-    writethumbnail: bool = typer.Option(True, "--writethumbnail", "-wt", help="Write thumbnail to disk"),
-    preferredquality: str = typer.Option("192", "--preferredquality", "-pq", help="Preferred audio quality(eg 256 or 320)"),
+    writethumbnail: bool = typer.Option(True, "--writethumbnail", "-wt", help="Write thumbnail to disk as a separate file"),
+    preferredquality: str = typer.Option("192", "--preferredquality", "-pq", help="Preferred audio quality ('192' or '256')"),
+    preferredcodec: str = typer.Option("mp3", "--preferredcodec", "-pc", help="Preferred audio codec (aac, m4a, mp3, mp4, ogg, wav, webm)"),
 ):
     '''Youtube MP3 download'''
     ydl_opts = {
@@ -15,7 +17,7 @@ def download_video(
         'writethumbnail': writethumbnail,
         'postprocessors': [{
             'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'm4a',
+            'preferredcodec': preferredcodec,
             'preferredquality': preferredquality,
         }],
         'outtmpl': '%(title)s.%(ext)s',
@@ -32,8 +34,13 @@ def download_video(
         'add_metadata': 'True',
     })
 
-    with YoutubeDL(ydl_opts) as ydl:
-        ydl.download([url])
+    try:
+        with YoutubeDL(ydl_opts) as ydl:
+            ydl.download([url])
+    except DownloadError as e:
+        typer.echo(f"Download failed: {e}")
+    except Exception as e:
+        typer.echo(f"An unexpected error occurred: {e}")
 
 if __name__ == "__main__":
     app()
